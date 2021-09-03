71cb8f4e0bbf0832bd2d7feee3043207c5e89b1a
wyh-123 <73167024+wyh-123@users.noreply.github.com>
2021-09-03 14:21:36 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
         const instance = axios.create({
-            baseURL: 'http://152.136.185.210:7878/api/m5',
+            baseURL: '接口添加coderwhy微信获取',
             timeout: 10000
         })
     /* axios的拦截器 */
